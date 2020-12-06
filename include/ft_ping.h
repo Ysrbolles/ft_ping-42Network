897a1666a102853b118aac0f7367be73c37bd66a
Ysrbolles <ysrbolles@gmail.com>
2020-12-06 13:15:23 +0100

    Create Socket

--- include/ft_ping.h
+++ include/ft_ping.h
@@ -6,7 +6,7 @@
 /*   By: ybolles <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 19:04:29 by ybolles           #+#    #+#             */
-/*   Updated: 2020/12/03 19:04:32 by ybolles          ###   ########.fr       */
+/*   Updated: 2020/12/06 10:06:30 by ybolles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@
 #include <netdb.h>
 #include <stdlib.h>
 #include <string.h>
+ #include <arpa/inet.h>
 #include "libft.h"
+#include "ip_icmp.h"
 
 #define PACKET_PING_SIZE 64
 #define HEADERLEN ICMP
@@ -29,14 +31,12 @@ typedef struct s_flags
         int FLAG_V;
         int FLAG_H;
         char *HOST;
-}               t_flags;
+} t_flags;
 
-typedef struct s_pingpacket
+struct ping_pkt 
 {
-
-        struct icmphdr *hdr;
-        //char msg[PACKET_PING_SIZE-sizeof(struct icmphdr)];
-
-} t_pingpacket;
-
+  
+   struct icmphdr hdr; 
+   char msg[PACKET_PING_SIZE - sizeof(struct icmphdr)]; 
+}g_pckt; 
 #endif
--- src/ft_ping.c
+++ src/ft_ping.c
@@ -35,20 +35,63 @@ int main(int ac, char **av)
 {
     if (ac > 2 && *av)
     {
-        int i;
-        int state;
-        char **tab;
+        // int i;
+        // int state;
 
-        i = 0;
-        if (*av && av[1][0] == '-')
+        // i = 0;
+        // if (*av && av[1][0] == '-')
+        // {
+        //     state = parse_flag(av[1][1]);
+        //     printf("%d\n", state);
+        // }
+
+        const char *hostName = "www.agoumi.com";
+        const char *portNumber = "https";
+        int ClientSocket;
+        struct addrinfo hints;
+        memset(&hints, 0, sizeof(struct addrinfo));
+        struct addrinfo *results;
+        struct addrinfo *record;
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_RAW;
+        hints.ai_protocol = IPPROTO_TCP;
+        int con;
+
+        if ((getaddrinfo(hostName, portNumber, &hints, &results)) != 0)
         {
-            state = parse_flag(av[1][1]);
-            printf("%d\n", state);
+            printf("Faied to translate Client Socket.\n");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            printf("-------> %d\n", results->ai_family);
+        }
+
+        record = results;
+        while (record != NULL)
+        {
+            ClientSocket = socket(record->ai_family, record->ai_socktype, record->ai_protocol);
+            printf("**********> %d\n", ClientSocket);
+            if (ClientSocket == -1)
+                continue;
+            printf(".......................\n");
+            con = connect(ClientSocket, record->ai_addr, record->ai_addrlen);
+            printf("//////////////> %d\n", con);
+            if (con != -1)
+                break;
+            close(ClientSocket);
+            record = record->ai_next;
+        }
+        if (record == NULL)
+        {
+            printf("Failed to create or connect Client Socket.\n");
+            exit(EXIT_FAILURE);
         }
     }
     else
     {
-        printf("khriwa\n");
+        fprintf(stderr, "Usage: %s port\n", av[0]);
+        exit(EXIT_FAILURE);
     }
     return 0;
 }
