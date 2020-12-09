224508a72eb582cc721ceb86e6608c25008af87f
Ysrbolles <ysrbolles@gmail.com>
2020-12-09 13:34:01 +0100

    Check IP

--- src/ft_ping.c
+++ src/ft_ping.c
@@ -6,7 +6,7 @@
 /*   By: ybolles <ybolles@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 16:38:07 by ybolles           #+#    #+#             */
-/*   Updated: 2020/12/08 21:55:25 by ybolles          ###   ########.fr       */
+/*   Updated: 2020/12/09 13:26:25 by ybolles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,19 +30,19 @@ int main(int ac, char **av)
         int rv;
 
         memset(&hints, 0, sizeof hints);
-        hints.ai_family = AF_INET;       // Set IP family to IPv4
+        hints.ai_family = AF_INET;    // Set IP family to IPv4
         hints.ai_socktype = SOCK_RAW; // Set socket type to stream
-        hints.ai_protocol = IPPROTO_TCP;
-        // hints.ai_protocol = IPPROTO_ICMP;
+        hints.ai_protocol = IPPROTO_ICMP;
+        hints.ai_flags = 0;
         printf("Host Name ------> %s\n", av[1]);
-        if ((rv = getaddrinfo(av[1], "https", &hints, &servinfo)) != 0)
+        if ((rv = getaddrinfo(av[1], NULL, &hints, &servinfo)) != 0)
         {
             fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
             exit(1);
         }
 
-        // loop through all the results and connect to the first we can
-        for (p = servinfo; p != NULL; p = p->ai_next)
+        p = servinfo;
+        while ((p->ai_next != NULL))
         {
             if ((sockfd = socket(p->ai_family, p->ai_socktype,
                                  p->ai_protocol)) == -1)
@@ -50,16 +50,9 @@ int main(int ac, char **av)
                 perror("socket");
                 continue;
             }
-
-            if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
-            {
-                perror("connect");
-                close(sockfd);
-                continue;
-            }
-
-            break; // if we get here, we must have connected successfully
         }
+
+        // loop through all the results and connect to the first we can
 
         if (p == NULL)
         {
@@ -67,8 +60,12 @@ int main(int ac, char **av)
             fprintf(stderr, "failed to connect\n");
             exit(2);
         }
+        printf("%d\n", sockfd);
         char str[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, p->ai_addr, str, p->ai_addrlen);
+        inet_pton(p->ai_family, av[1], &(p->ai_addr));
+        printf(" ---------> %s\n", str);
+        inet_ntop(p->ai_family, &(p->ai_addr), str, sizeof(str));
+
         printf(" ---------> %s\n", str);
     }
 }
