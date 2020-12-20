d4fe9fff04b52a43643b74642c20e8f45c62a9c3
Ysrbolles <ysrbolles@gmail.com>
2020-12-20 19:17:11 +0100

    merging

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 
 
-
 #include "ft_ping.h"
 
 t_params g_params;
@@ -13,26 +12,36 @@ int main(int ac, char **av)
 	init_params();
 	if (getuid() != 0)
 		printf("ft_ping: Operation not permitted\n");
+	if (ac == 1)
+	{
+		printf("usage: ./ft_ping [-vh] hostname\n");
+		exit(0);
+	}
+	if (ac == 2 && av[1] == "-")
+	{
+		printf("usage: ./ft_ping [-vh] hostname\n");
+		exit(0);
+	}
 	if (ac != 2)
 	{
 		printf("Usage ERROR \n");
 		exit(1);
 	}
 	else
-	{	
-		g_params.Host = av[1];
-		if ((g_params.ClientSocket = create_sock()) == -1)
-			printf("Socket Failed\n");
-		inet_ntop(g_params.addr_info->ai_family, &((struct sockaddr_in *)(void *)g_params.addr_info->ai_addr)->sin_addr, g_params.addrstr, sizeof(g_params.addrstr));
-		printf("PING %s (%s) %zu(%zu) bytes of data.\n",
-			   av[1], g_params.addrstr,
-			   56, 58);
-		start_signal();
-		while (g_params.pingloop)
-		{
-			get_packet();
-			ft_sleep(2);
-		}
+	{
+		// g_params.Host = av[1];
+		// if ((g_params.ClientSocket = create_sock()) == -1)
+		// 	printf("Socket Failed\n");
+		// inet_ntop(g_params.addr_info->ai_family, &((struct sockaddr_in *)(void *)g_params.addr_info->ai_addr)->sin_addr, g_params.addrstr, sizeof(g_params.addrstr));
+		// printf("PING %s (%s) %zu(%zu) bytes of data.\n",
+		// 	   av[1], g_params.addrstr,
+		// 	   56, 58);
+		// start_signal();
+		// while (g_params.pingloop)
+		// {
+		// 	get_packet();
+		// 	ft_sleep(2);
+		// }
 	}
 
 	return (0);
