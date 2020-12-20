02444d5c2a68de1119a9a8c39ee26bd066769ac2
Ysrbolles <ysrbolles@gmail.com>
2020-12-20 09:10:43 +0100

    double

--- src/packet.c
+++ src/packet.c
@@ -6,7 +6,7 @@
 /*   By: ybolles <ybolles@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/06 20:28:18 by ybolles           #+#    #+#             */
-/*   Updated: 2020/12/20 09:08:13 by ybolles          ###   ########.fr       */
+/*   Updated: 2020/12/20 09:10:29 by ybolles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,13 @@ unsigned short checksum(void *b, int len)
 int send_packet()
 {
 	int sending;
-	gettimeofday(&g_params.time_start, NULL);
 	bzero(&g_params.pkt, sizeof(g_params.pkt));
 	g_params.pkt.hdr.type = ICMP_ECHO; 
 	g_params.pkt.hdr.un.echo.id = getpid();
 	g_params.pkt.hdr.un.echo.sequence = g_params.msg_count++;
 	g_params.pkt.hdr.checksum = checksum(&g_params.pkt, sizeof(g_params.pkt));
 	g_params.msg_count == 1 ? gettimeofday(&g_params.tfs, NULL) : 0;
+	gettimeofday(&g_params.time_start, NULL);
 	if (sendto(g_params.ClientSocket, &g_params.pkt, sizeof(g_params.pkt), 0, g_params.addr_info->ai_addr, g_params.addr_info->ai_addrlen) <= 0)
 		g_params.flag = g_params.flag_v ? g_params.flag : 0;
 }
@@ -53,7 +53,7 @@ long double calc(struct timeval start, struct timeval end)
 	long double startII = end.tv_sec - start.tv_sec;
 	long double endII = end.tv_usec - start.tv_usec;
 
-	res = (long double)((startII * 1000.) + endII / 1000. );
+	res = (long double)((startII * 1000.) + (endII / 1000.) );
 	printf("------> res %Lf\n", res);
 	return (res);
 }
